87cdb606e0ecdb1a3f84e518126cc2953944ae02
Slimboyfat-1979 <69854909+Slimboyfat-1979@users.noreply.github.com>
2024-06-09 15:25:39 +1200

    Changes

--- lessons.php
+++ lessons.php
@@ -35,26 +35,49 @@
 <section>
     <div class="faq">
         <h2>Frequently Asked Questions</h2>
-        <div class="accordion">
-            <div class="accordion-item">
-                <div class="accordion-title">When can I start lessons?</div>
-                <div class="accordion-content">Individual lessons coincide with the school term, however you can start anytime during the term. Because our lessons are very popular it is recommended you contact us as soon as possible to secure a day and time, if nothing is currently available we can pop you on our wait list and contact you as soon as a space opens.</div>
+        <div class="container">
+            <div class="item">
+                <a href="#tab1" class="title">When can I start lessons?</a>
+                <div id="tab1"  class="content">
+                    <p>Individual lessons coincide with the school term, however you can start anytime during the term. Because our lessons are very popular it is recommended you contact us as soon as possible to secure a day and time, if nothing is currently available we can pop you on our wait list and contact you as soon as a space opens.</p>
+                </div>
             </div>
-            <div class="accordion-item">
-                <div class="accordion-title">How and when do I pay?</div>
-                <div class="accordion-content">Our preferred option is to invoice you for the term, payment is due by the 20th of the month following date of invoice, you can either pay online or in store. If you are unsure, there is an option to have a trial lesson priced at $45 for a half hour or $64 for a full hour.</div>
+            <div class="item">
+                <a href="#tab2" class="title">How And When Do I Pay?</a>
+                <div id="tab2" class="content">
+                    <p>Our preferred option is to invoice you for the term, payment is due by the 20th of the month following date of invoice, you can either pay online or in store. If you are unsure, there is an option to have a trial lesson priced at $45 for a half hour or $64 for a full hour.</p>
+                </div>
             </div>
-            <div class="accordion-item">
-                <div class="accordion-title">What days and times are available?</div>
-                <div class="accordion-content">Generally, individual lessons are held Monday - Friday from 2pm until 8pm, some teachers are available on Saturday.</div>
-            </div>
-            <div class="accordion-item">
-                <div class="accordion-title">How many lessons should I have per week?</div>
-                <div class="accordion-content">It is recommended you have one lesson per week, so you have time to practice what you have been taught..</div>
+            <div class="item">
+                <a href="#tab3" class="title">What days and times are available?</a>
+                <div id="tab3"  class="content">
+                    <p>Generally, individual lessons are held Monday - Friday from 2pm until 8pm, some teachers are available on Saturday.</p>
+                </div>
             </div>
         </div>
-    </div>
 </section>
+<script>
+
+</script>
 
 
 <?php require_once ('./footer.php') ?>
+
+<!--<div class="accordion">-->
+<!--    <div class="accordion-item">-->
+<!--        <div class="accordion-title">When can I start lessons?</div>-->
+<!--        <div class="accordion-content">Individual lessons coincide with the school term, however you can start anytime during the term. Because our lessons are very popular it is recommended you contact us as soon as possible to secure a day and time, if nothing is currently available we can pop you on our wait list and contact you as soon as a space opens.</div>-->
+<!--    </div>-->
+<!--    <div class="accordion-item">-->
+<!--        <div class="accordion-title">How and when do I pay?</div>-->
+<!--        <div class="accordion-content">Our preferred option is to invoice you for the term, payment is due by the 20th of the month following date of invoice, you can either pay online or in store. If you are unsure, there is an option to have a trial lesson priced at $45 for a half hour or $64 for a full hour.</div>-->
+<!--    </div>-->
+<!--    <div class="accordion-item">-->
+<!--        <div class="accordion-title">What days and times are available?</div>-->
+<!--        <div class="accordion-content">Generally, individual lessons are held Monday - Friday from 2pm until 8pm, some teachers are available on Saturday.</div>-->
+<!--    </div>-->
+<!--    <div class="accordion-item">-->
+<!--        <div class="accordion-title">How many lessons should I have per week?</div>-->
+<!--        <div class="accordion-content">It is recommended you have one lesson per week, so you have time to practice what you have been taught..</div>-->
+<!--    </div>-->
+<!--</div>-->

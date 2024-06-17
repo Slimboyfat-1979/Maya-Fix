2e95d3a01d5c9adac8b1f87c811aad3a20381938
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2024-06-17 20:31:31 +1200

    Made Adjustments

--- mailer.php
+++ mailer.php
@@ -22,7 +22,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mail->Host = 'smtp.titan.email';                     //Set the SMTP server to send through
         $mail->SMTPAuth = true;                                   //Enable SMTP authentication
         $mail->Username = 'info@lookoutdigital.co.nz';                     //SMTP username
-        $mail->Password = 'YouLookLikeAThing1979!!';                               //SMTP password
+        $mail->Password = '';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
